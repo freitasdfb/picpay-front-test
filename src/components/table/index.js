a63f99a09086530ecf3f17ff7db53209e4450c20
Victor Barbosa <freitasdfb@gmail.com>
2022-02-10 03:16:42 -0300

    :construction: Head da tabela

--- src/components/table/index.js
+++ src/components/table/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import { PicTable } from './styles'
+import { PicTable, TextField } from './styles'
 import { Card } from 'react-bootstrap'
 
 function TablePic() {
 
   return (
-    <Card style={{marginTop: '2%'}}>
+    <Card style={{ marginTop: '2%' }}>
       <PicTable>
-        <thead>
-          Dois heads na tabel
+        <thead style={{ padding: '0px 2%'}}>
+          <th>
+            <TextField placeholder="Pesquisar por usuário" />
+          </th>
         </thead>
-        <thead>
+        <thead id="mainHead">
           <tr>
             <th>Usuário</th>
             <th>Título </th>
@@ -26,17 +28,23 @@ function TablePic() {
             <td>Mark</td>
             <td>Otto</td>
             <td>@mdo</td>
+            <td></td>
+            <td></td>
           </tr>
           <tr>
             <td>2</td>
             <td>Jacob</td>
             <td>Thornton</td>
             <td>@fat</td>
+            <td></td>
+            <td></td>
           </tr>
           <tr>
             <td>3</td>
             <td colSpan={2}>Larry the Bird</td>
             <td>@twitter</td>
+            <td></td>
+            <td></td>
           </tr>
         </tbody>
       </PicTable>
--- src/components/table/styles.js
+++ src/components/table/styles.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { Form } from 'react-bootstrap';
 
 export const PicTable = styled.table`
   @import url('https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap');
@@ -6,6 +7,10 @@ export const PicTable = styled.table`
     padding: 10px;
   }
 
+  #mainHead th {
+    border-bottom: 2px solid #CFDCE5;
+  }
+  
   th { 
     font-family: 'Lato', sans-serif;
     font-size: 14px;
@@ -13,7 +18,6 @@ export const PicTable = styled.table`
     font-weight: 700;
     line-height: 17px;
     text-align: left;
-    border-bottom: 2px solid #CFDCE5;
     padding: 10px 30px;
   }
 
@@ -22,4 +26,12 @@ export const PicTable = styled.table`
       background-color: #F5F8FA;
   }
 
+`
+
+export const TextField = styled(Form.Control)`
+  background: none; 
+
+  &:focus { 
+    background: none;
+  }
 `
